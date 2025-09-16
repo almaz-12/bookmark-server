f75bc23c7f11046985beb418293637d505383c74
Almaz Khabibullin <adreams2016@yandex.ru>
2025-09-16 19:55:00 +0300

    фикс

--- models/Category.js
+++ models/Category.js
@@ -1,36 +1,47 @@
 const db = require('../database/db');
 
 class Category {
-    static getAll(callback) {
-        db.all(`
+  static getAll(callback) {
+    db.all(
+      `
             SELECT c.*,
-                   (SELECT COUNT(*) FROM bookmarks WHERE category_id = c.id) as bookmark_count
+                   (SELECT COUNT(*) FROM bookmarks WHERE category_id = c.id) as bookmarks
             FROM categories c
             ORDER BY c.name
-        `, callback);
-    }
+        `,
+      callback
+    );
+  }
 
-    static getById(id, callback) {
-        db.get('SELECT * FROM categories WHERE id = ?', [id], callback);
-    }
+  static getById(id, callback) {
+    db.get('SELECT * FROM categories WHERE id = ?', [id], callback);
+  }
 
-    static create(name, alias, callback) {
-        db.run('INSERT INTO categories (name, alias) VALUES (?, ?)', [name, alias], function(err) {
-            callback(err, { id: this.lastID, name, alias });
-        });
-    }
+  static create(name, alias, callback) {
+    db.run(
+      'INSERT INTO categories (name, alias) VALUES (?, ?)',
+      [name, alias],
+      function (err) {
+        callback(err, { id: this.lastID, name, alias, bookmarks: null });
+      }
+    );
+  }
 
-    static update(id, name, alias, callback) {
-        db.run('UPDATE categories SET name = ?, alias = ? WHERE id = ?', [name, alias, id], function(err) {
-            callback(err, this.changes);
-        });
-    }
+  static update(id, name, alias, callback) {
+    db.run(
+      'UPDATE categories SET name = ?, alias = ? WHERE id = ?',
+      [name, alias, id],
+      function (err) {
+        callback(err, this.changes);
+      }
+    );
+  }
 
-    static delete(id, callback) {
-        db.run('DELETE FROM categories WHERE id = ?', [id], function(err) {
-            callback(err, this.changes);
-        });
-    }
+  static delete(id, callback) {
+    db.run('DELETE FROM categories WHERE id = ?', [id], function (err) {
+      callback(err, this.changes);
+    });
+  }
 }
 
 module.exports = Category;
